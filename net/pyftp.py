625412d5b460fc8213ccfe44f7694faba120a247
曹宇 <caoyu1099@gmail.com>
2011-10-17 22:58:08 +0800

    handle error

--- net/pyftp.py
+++ net/pyftp.py
@@ -16,55 +16,65 @@ def getargs():
 
 title = "pyftp>"
 
-def login():
+def connect():
     getargs()
-    try:
-        global pyftp
-        pyftp = ftplib.FTP(host, username, passwd)
-        print "Login Successful"
-        pyftp.getwelcome()
+    global pyftp
+    pyftp = ftplib.FTP(host, username, passwd)
+    print "Login Successful!"
+    print pyftp.getwelcome()
+
+def operate():
+    while True:
         print title,
-        while True:
-            stdin = raw_input()
-            stdarg = stdin.split(" ")
-            if stdin == 'ls' or stdin == 'dir':
-                pyftp.dir()
-            elif stdin == 'pwd':
-                pyftp.pwd()
-            elif stdarg[0] == 'cd':
-                pyftp.cwd(stdarg[1])
-            elif stdarg[0] == 'mkdir':
-                pyftp.mkd(stdarg[1])
-            elif stdarg[0] == 'rmdir':
-                pyftp.rmd(stdarg[1])
-            elif stdarg[0] == 'mv' or stdarg[0] == 'rename':
-                pyftp.rename(stdarg[1],stdarg[2])
-            elif stdarg[0] == 'rm' or stdarg[0] == 'delete':
-                pyftp.delete(stdarg[1])
-            elif stdarg[0] == 'get':
-                local_file = open(stdarg[1], 'wb')
-                pyftp.retrbinary('RETR %s' % stdarg[1], local_file.write)
-                print "Transfer complete."
-            elif stdarg[0] == 'put':
-                local_file = open(stdarg[1], 'rb')
-                pyftp.storbinary('STOR %s' % stdarg[1], local_file)
-                print "Transfer complete."
-            elif stdin == 'quit' or stdin == 'exit':
-                pyftp.quit()
-                break
-            elif stdin == 'help':
-                print "ls\tdir\tpwd\tmkdir\t\nrmdir\tmv\trename\tquit\t\nexit\tdelete\trm\tget"
-            elif stdin == '':
-                pass
-            else:
-                print "Invalid command.Try again.\"help\" for more information."
-            print title,
+        stdin = raw_input()
+        stdarg = stdin.split(" ")
+        if stdin == 'ls' or stdin == 'dir':
+            pyftp.dir()
+        elif stdin == 'pwd':
+            print pyftp.pwd()
+        elif stdarg[0] == 'cd':
+            pyftp.cwd(stdarg[1])
+        elif stdarg[0] == 'mkdir':
+            pyftp.mkd(stdarg[1])
+        elif stdarg[0] == 'rmdir':
+            pyftp.rmd(stdarg[1])
+        elif stdarg[0] == 'mv' or stdarg[0] == 'rename':
+            pyftp.rename(stdarg[1],stdarg[2])
+        elif stdarg[0] == 'rm' or stdarg[0] == 'delete':
+            pyftp.delete(stdarg[1])
+        elif stdarg[0] == 'get':
+            local_file = open(stdarg[1], 'wb')
+            pyftp.retrbinary('RETR %s' % stdarg[1], local_file.write)
+            print "Transfer complete."
+        elif stdarg[0] == 'put':
+            local_file = open(stdarg[1], 'rb')
+            pyftp.storbinary('STOR %s' % stdarg[1], local_file)
+            print "Transfer complete."
+        elif stdin == 'quit' or stdin == 'exit':
+            pyftp.quit()
+            print "Goodbye!"
+            break
+        elif stdin == 'help':
+            print "ls\tdir\tpwd\tmkdir\t\nrmdir\tmv\trename\tquit\t\nexit\tdelete\trm\tget"
+        elif stdin == '':
+            pass
+        else:
+            print "Invalid command.Try again.\"help\" for more information."
+
+def cycle():
+    try:
+        operate()
     except ftplib.all_errors:
-        print ftplib.all_errors
-        print "Error\r\nLogout"
+        cycle()
+    except IndexError:
+        cycle()
+    except KeyboardInterrupt, KeyError:
+        pass
+        print "Goodbye!"
 
 def main():
-    login()
+    connect()
+    cycle()
 
 if __name__ == '__main__':
     main()
