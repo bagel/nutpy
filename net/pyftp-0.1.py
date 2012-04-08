34c18d9304aa14ce48601206e430071aefc6bffb
bagel <freetgm@gmail.com>
2012-04-08 15:32:11 +0800

    add port mode and change msg

--- net/pyftp-0.1.py
+++ net/pyftp-0.1.py
@@ -9,92 +9,91 @@ if len(sys.argv) != 2 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
     print usage
     sys.exit(0)
 
+MODE = 0   #'1' use pasv mode, else use port mode.
+
 title = "pyftp>"
 
-msg = ["Goodbye!",
+inf = ("Goodbye!",
        "Error: file name not given or give too more.",
        "Command not found, try 'h' or 'help' for more help.",
        "Error: address can't connect, please check and try again.",
        "Error: please check and try again.",
        "Local Error: no such file or directory",
        "Task not complete.",
-       "Login Error: please check your user or password and try again."]
+       "Login Error: please check your user or password and try again.")
 
-def recive(ftp):
-    global ret
-    ret = ftp.recv(1024)
-    print ret,
+def get_msg():
+    global msg
+    ref = cftp.makefile('rb')
+    msg = ref.readline()
+    rn = msg[:3]
+    if msg[3:4] == '-':
+        while True:
+            next_msg = ref.readline()
+            msg = msg + '\n' + next_msg
+            if next_msg[:3] == rn and next_msg[3:4] != '-':
+                break
+    print msg,
+    return msg
 
-def cyc_recive(ftp):
-    global cret
+def get_data(s):
     while True:
-        cret = ftp.recv(1024)
-        if cret == '':
+        data = s.recv(4096)
+        if data == '':
             break
-        print cret,
+        print data,
 
-def recv_num(r):
-    return int(r.split(' ')[0])
-    
 def send_cmd(command):
     cftp.send(command + '\n')
-    recive(cftp)
+    get_msg()
 
 def login():
     global cftp
     cftp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cftp.connect((sys.argv[1], 21))
+    get_msg()
     user = raw_input("user(ftp):")
     if user == '':
         user = 'ftp'
-    password = raw_input("password:")
     send_cmd("USER %s" % user)
-    send_cmd("PASS %s" % password)
-    recive(cftp)
-    if recv_num(ret) == 530:
-        print "%s\n%s" % (msg[7], msg[0])
+    password = raw_input("password:")
+    if msg[0] == '3':
+        send_cmd("PASS %s" % password)
+#    if recv_num(msg) != 230:
+#        get_msg()
+    if msg[:3] == '530':
+        print "%s\n%s" % (inf[7], inf[0])
         sys.exit(0)
 
-def data_connect(r):
-    imin = r.find('(')
-    imax = r.find(')')
+def pasv_connect():
+    imin = msg.find('(')
+    imax = msg.find(')')
     imin = imin + 1
-    num_list = r[imin:imax].split(',')
+    num_list = msg[imin:imax].split(',')
     ip = "%s.%s.%s.%s" % (num_list[0], num_list[1], num_list[2], num_list[3])
     port = int(num_list[4]) * 256 + int(num_list[5])
-    global dftp
-    dftp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    dftp.connect((ip, port))
+    global sftp
+    sftp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sftp.connect((ip, port))
 
 def input_cmd():
     global cmd, cmd0, cmd_len, mv_cmd1, mv_cmd2, filename
     cmd = raw_input("%s" % title)
     scmd = cmd.split(' ')
     cmd_len = len(scmd)
-    scmd[0] = scmd[0].lower()
     cmd0 = scmd[0]
 
-    if scmd[0] == 'ls':
-        scmd[0] = 'list'
-    elif scmd[0] == 'cd':
-        scmd[0] = 'cwd'
-    elif scmd[0] == 'mkdir':
-        scmd[0] = 'mkd'
-    elif scmd[0] == 'rmdir':
-        scmd[0] = 'rmd'
-    elif scmd[0] == 'rm':
-        scmd[0] = 'dele'
-    elif scmd[0] == 'get' and cmd_len == 2:
-        scmd[0] = 'retr'
-        filename = scmd[1]
-    elif scmd[0] == 'put' and cmd_len == 2:
-        scmd[0] = 'stor'
+    if scmd[0] == 'get' or scmd[0] == 'put' and cmd_len == 2:
         filename = scmd[1]
 
-    if cmd_len == 2:
-        cmd = scmd[0] + ' ' + scmd[1]
-    else:
-        cmd = scmd[0]
+    cmd_dict = {'ls':'list', 'cd':'cwd', 'mkdir':'mkd', 'rmdir':'rmd',
+                'rm':'dele', 'get':'retr', 'put':'stor'}
+    if scmd[0] in cmd_dict.keys():
+        scmd[0] = cmd_dict[scmd[0]]
+
+    cmd = scmd[0]
+    for i in range(1, cmd_len):
+        cmd = cmd + ' ' + scmd[i]
 
     if scmd[0] == 'mv' and cmd_len == 3:
         mv_cmd1 = 'rnfr' + ' ' + scmd[1]
@@ -102,13 +101,55 @@ def input_cmd():
 
 def mod_pasv():
     send_cmd('pasv')
-    data_connect(ret)
-    send_cmd(cmd)
-#    recive(cftp)
+    if msg[:3] == '227':
+        pasv_connect()
+        send_cmd(cmd)
+        return 1
+    return 0
+#    get_msg(cftp)
+
+def make_port():
+    import random
+    global port, port1, port2
+    port = random.randint(1024,65535)
+    port1 = port/256
+    port2 = port%256
+
+def port_connect():
+    global port_cmd, tftp
+    hostip = socket.gethostbyname(socket.gethostname()) #replace to your host ip here
+    ipn = hostip.split('.')
+    port_cmd = "PORT %s,%s,%s,%s,%s,%s" % (ipn[0], ipn[1], ipn[2], ipn[3], port1, port2)
+    tftp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tftp.bind((hostip, port))
+    tftp.listen(1)
+
+def mod_port():
+    global tftp_sock
+    make_port()
+    port_connect()
+    send_cmd(port_cmd)
+    if msg[0] == '2':
+        send_cmd(cmd)
+        tftp_sock, addr = tftp.accept()
+        return 1
+    return 0
+
+def chs_mod():
+    global sock
+    if MODE == 1:
+        if mod_pasv():
+            sock = sftp
+            return 1
+    else:
+        if mod_port():
+            sock = tftp_sock
+            return 1
+    return 0
 
 def get_file(filename):
-    mod_pasv()
-    if recv_num(ret) == 550:
+    chs_mod()
+    if msg[:3] == '550':
         return 0
     fd = open(filename, 'wb+')
     print "Downloading..."
@@ -122,30 +163,28 @@ def get_file(filename):
 
 def put_file(filename):
     fp = open(filename, 'rb+')
-    mod_pasv()
+    chs_mod()
     while True:
         data = fp.readline()
         if not data:
             break
         dftp.send(data)
+        print data
     fp.close()
 
-cmd_list = ['ls', 'cd', 'pwd', 'rm', 'rmdir', 'mkdir', 'mv', 'get', 'put', 'help', 'quit']
+cmd_list = ('!', 'ls', 'cd', 'pwd', 'rm', 'rmdir', 'mkdir', 'mv', 'get', 'put', 'help', 'quit')
 def put_help():
     cmd_len = len(cmd_list)
-    i = 0
-    while (i < cmd_len):
+    for i in range(cmd_len):
         print "%s " % cmd_list[i],
-        i = i + 1
     print
-
 
 def cyc_run():
     while True:
         input_cmd()
 
         if cmd == "q" or cmd == "quit" or cmd == "exit":
-            print msg[0]
+            print inf[0]
             sys.exit(0)
         elif cmd == '':
             continue
@@ -154,32 +193,36 @@ def cyc_run():
             continue
         elif cmd0 == 'mv':
             if cmd_len != 3:
-                print msg[1]
+                print inf[1]
                 continue
             send_cmd(mv_cmd1)
             send_cmd(mv_cmd2)
             continue
         elif cmd0 == 'ls':
-            mod_pasv()
-            cyc_recive(dftp)
-            recive(cftp)   #cmd complete server send msg
+            if chs_mod():
+                get_data(sock)
+                get_msg()    #server send complete msg
             continue
         elif cmd0 == 'get':
             if cmd_len != 2:
-                print msg[1]
+                print inf[1]
                 continue
             if get_file(filename):
-                recive(cftp)
+                get_msg()
             continue
         elif cmd0 == 'put':
             if cmd_len != 2:
-                print msg[1]
+                print inf[1]
                 continue
             put_file(filename)
-        #    recive(cftp)
+        #    get_msg()
+            continue
+        elif cmd0[0] is '!':
+            import os
+            os.system('%s' % cmd[1:])
             continue
         elif cmd0 not in cmd_list:
-            print msg[2]
+            print inf[2]
             continue
 
         send_cmd(cmd)
@@ -190,17 +233,17 @@ def main():
         while True:
             try:
                 cyc_run()
-            except KeyboardInterrupt:
-                print "\n%s\n%s" % (msg[6],msg[0])
-                sys.exit(0)
-            except IndexError:
-                print msg[4]
-            except IOError:
-                print "%s: '%s'" % (msg[5], filename)
+#            except KeyboardInterrupt:
+#                print "\n%s\n%s" % (inf[6],inf[0])
+#                sys.exit(0)
+            except IndexError, NameError:
+                print inf[4]
     except socket.gaierror:
-        print "%s\n%s" % (msg[3], usage)
-    except KeyboardInterrupt:
-        print "\n%s\n%s" % (msg[6], msg[0])
+        print "%s\n%s" % (inf[3], usage)
+#    except KeyboardInterrupt:
+#        print "\n%s\n%s" % (inf[6], inf[0])
+#    except:
+#        sys.exit(0)
 
 if __name__ == "__main__":
     main()
